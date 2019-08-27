3937db89852f9dea5040313ae41db5f33cc62909
Diogo <drd.rclrd@gmail.com>
2019-08-27 13:56:07 -0300

    created function to handle language

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 import { withNamespaces } from 'react-i18next';
+import i18n from './i18n';
 
 function App ({ t }) {
-  return <h1>{t('Welcome to React')}</h1>
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
+
+  return (
+    <div>
+      <button onClick={() => changeLanguage('de')}>de</button>
+      <button onClick={() => changeLanguage('en')}>en</button>
+      <h1>{t('Welcome to React')}</h1>
+    </div>
+  )
 }
 
 export default withNamespaces()(App);
