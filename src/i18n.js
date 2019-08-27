f0839c6b029b75b415d502c26616b8803135c51d
Diogo <drd.rclrd@gmail.com>
2019-08-27 13:56:45 -0300

    add language

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,26 @@
 import i18n from 'i18next';
 import { reactI18nextModule } from 'react-i18next';
+import detector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en/translation.json';
+import translationDE from './locales/de/translation.json';
 
 const resources = {
   en: {
     translation: translationEN
+  },
+  de: {
+    translation: translationDE
   }
 };
 
 i18n
+  .use(detector)
   .use(reactI18nextModule)
   .init({
     resources,
     lng: "en",
+    fallbackLng: "en", 
 
     keySeparator: false,
 
